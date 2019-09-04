64c7bdd74fc46229f13ff3df35ba8edc13ff49f7
BIJINI CB <40457704+BIJINICB@users.noreply.github.com>
2019-09-04 15:44:43 +0530

    Create TimeConvert2.py

---
+++ TimeConvert2.py
@@ -0,0 +1,14 @@
+dates = [
+    '1 week, 6 days, 11 hours, 20 mins, 13 secs ago',
+    '1 week, 10 hours ago',
+    '1 week, 1 day ago',
+    '6 days, 11 hours, 20 mins ago',
+    '1 hour ago',
+    '11 hours, 20 mins ago',
+    '20 mins 10 secs ago',
+    '10 secs ago',
+    '1 sec ago',
+]    
+
+for date in dates:
+    print(convert_datetime(date))
